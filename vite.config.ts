1f781279afc7e67e89c3dbbf048d4cf818c3fb54
livedder44 <60624850+livedder44@users.noreply.github.com>
2025-11-07 11:20:56 +0200

    fix: add Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/stc-home/', // 👈 додай цей рядок
   plugins: [react()],
   resolve: {
     alias: {
